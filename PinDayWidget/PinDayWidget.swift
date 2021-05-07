8018e9c6856a1d637d7c6d4996a4089a8201c413
shiba1014 <shiba.png@gmail.com>
2021-05-07 22:42:06 +0900

    Display selected event

--- PinDayWidget/PinDayWidget.swift
+++ PinDayWidget/PinDayWidget.swift
@@ -22,22 +22,22 @@ struct Provider: IntentTimelineProvider {
 
     func placeholder(in context: Context) -> EventEntry {
 
-        EventEntry(date: Date(), event: .mock, configuration: SelectEventIntent())
+        EventEntry(date: Date(), event: .placeholder, configuration: SelectEventIntent())
     }
 
     func getSnapshot(for configuration: SelectEventIntent, in context: Context, completion: @escaping (EventEntry) -> ()) {
-        let entry = EventEntry(date: Date(), event: .mock, configuration: configuration)
+        let entry = EventEntry(date: Date(), event: .snapshot, configuration: configuration)
         completion(entry)
     }
 
     func getTimeline(for configuration: SelectEventIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [EventEntry] = []
-
+        let event = events.first { $0.id.uuidString == configuration.event?.identifier } ?? .snapshot
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = EventEntry(date: entryDate, event: events.first, configuration: configuration)
+            let entry = EventEntry(date: entryDate, event: event, configuration: configuration)
             entries.append(entry)
         }
 
@@ -53,7 +53,18 @@ struct EventEntry: TimelineEntry {
 }
 
 extension Event {
-    static let mock: Event = {
+    static let placeholder: Event = {
+        let event = Event.init(entity: Event.entity(), insertInto: nil)
+        let date = Date().fixed(month: 12, day: 31).beginning()
+        event.id = UUID()
+        event.title = "\(date.year)"
+        event.pinnedDate = date
+        event.backgroundColor = Data.encode(color: .appOrange)
+        event.createdAt = Date()
+        return event
+    }()
+
+    static let snapshot: Event = {
         let event = Event.init(entity: Event.entity(), insertInto: nil)
         let date = Date().fixed(month: 12, day: 31).beginning()
         event.id = UUID()
